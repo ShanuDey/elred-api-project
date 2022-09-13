1c447dfa1d279496f10fc2093d147a05f83925f8
ShanuDey <sd4shanudey@gmail.com>
2022-09-13 16:08:51 +0530

    created user schema and model with mongoose
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

---
+++ model/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: { type: String, unique: true },
+  password: String,
+  token: String,
+});
+
+module.exports = mongoose.model('User', UserSchema);
