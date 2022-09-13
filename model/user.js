76c98c1f4860f03bca097dc6e3b34b46c65f10d8
ShanuDey <sd4shanudey@gmail.com>
2022-09-13 19:18:00 +0530

    update user model with tasks array
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Task = require("./task");
 
 const UserSchema = mongoose.Schema({
   first_name: String,
@@ -6,6 +7,7 @@ const UserSchema = mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   token: String,
+  tasks: [Task.schema]
 });
 
 module.exports = mongoose.model("User", UserSchema);
