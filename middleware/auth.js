5e09f34a6bae53fe73018c8531d2178700c4e824
ShanuDey <sd4shanudey@gmail.com>
2022-09-13 17:46:13 +0530

    created an auth middleware for verifyToken
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  // get the token from the request
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  
+  // check if the token if present
+  if(!token) {
+    return res.status(403).send("Token is not present for authorization");
+  }
+
+  // verify the token with the key
+  try{
+    const decodedPayload = jwt.verify(token, process.env.TOKEN_KEY);
+    req.user = decodedPayload
+  }
+  catch{
+    return res.status(401).send("Token is not valid");
+  }
+  return next();
+}
+
+module.exports = verifyToken;
