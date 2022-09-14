2090c1a42e7d8d7e39cc69583c437ffbfbd2ea81
ShanuDey <sd4shanudey@gmail.com>
2022-09-14 17:19:22 +0530

    heroku has PORT environment variable exposed
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.API_PORT || "8000";
+const port = process.env.PORT || "8000";
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
